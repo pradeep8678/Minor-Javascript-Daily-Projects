7e712a7abb6a9f2325d58d7a868c0b652012bb47
Pradip Singh <pradeepsinghania906@gmail.com>
2024-06-03 22:23:44 +0530

    Add files via upload

---
+++ Analog Clock/Weather App/script.js
@@ -0,0 +1,10 @@
+const numberHours = document.querySelectorAll('.number-hours');
+
+const numberElement = [];
+
+for (let i = 0; i <= 12; i++) {
+    numberElement.push(
+
+    );
+    console.log(numberElement);
+}
